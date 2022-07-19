a6d47c2e636df92b68437710de0a51d4eba5f350
016779631 <victoria.resende1010@gmail.com>
2022-07-19 16:28:39 -0300

    version 4

--- app/src/main/java/com/example/dramalist/Profile.java
+++ app/src/main/java/com/example/dramalist/Profile.java
@@ -5,24 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
-public class TelaPrincipal extends AppCompatActivity {
+public class Profile extends AppCompatActivity {
 
-    private View profile;
+    private Button bt_sair;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tela_principal);
+        setContentView(R.layout.activity_profile);
 
         getSupportActionBar().hide();
 
-        profile = findViewById(R.id.containerUser);
+        bt_sair = findViewById(R.id.bt_sair);
 
-        profile.setOnClickListener(new View.OnClickListener() {
+        bt_sair.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(TelaPrincipal.this, Profile.class);
+                Intent intent = new Intent(Profile.this, FormLogin.class);
                 startActivity(intent);
             }
         });
